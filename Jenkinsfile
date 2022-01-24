8b4a5bf3fd949f10dcf21d5a319ee8347b4fc9be
Jonay Hernández Izquierdo <55432892+lawd17@users.noreply.github.com>
2022-01-24 18:27:00 +0000

    Add files via upload

---
+++ Jenkinsfile
@@ -0,0 +1,3 @@
+git clone this repository
+sudo docker build -t hello-word-php-apache .
+sudo docker run -p 81:80 hello-word-php-apache
---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+$nombreImg = 'mario.png';
+
+echo "<h1>Página principal con PHP de Jonay</h1>";
+echo "<img src='./img/$nombreImg' alt='imagen_mario'>";
+?>
